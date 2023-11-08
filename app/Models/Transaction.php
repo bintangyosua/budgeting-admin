f7b9b914bb80c06138d282721092ff5c642f8ed8
Casul63 <ikhsanhafidz09@gmail.com>
2023-11-08 17:28:05 +0700

    Menambah relasi di model

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -8,4 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class Transaction extends Model
 {
     use HasFactory;
+
+    public function wallet()
+    {
+        return $this->belongsTo(Wallet::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
